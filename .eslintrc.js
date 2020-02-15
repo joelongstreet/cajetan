928ac6eb837323a5a8b0d33483cbdbd221ed25d8
Joe Longstreet <joelongstreet@gmail.com>
2020-02-15 14:39:07 -0600

    lint and nvm versioning

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'key-spacing': 0,
+    'no-console': 0
+  },
+};
