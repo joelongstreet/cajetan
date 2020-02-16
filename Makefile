662d85d318f24431a0c0f3ee4b8500fa8ab0327f
Joe Longstreet <joelongstreet@gmail.com>
2020-02-16 09:58:22 -0600

    seed team search terms

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ MIGRATION_TASKS=`cat $(MIGRATION_FILE)`
 migrate:
 	docker exec -i longshot psql -U postgres -e < migrate.sql
 
+seed:
+	docker exec -i longshot psql -U postgres -e < seed.sql
+
 start:
 	docker-compose up -d $(ARG)
 	npm install
--- migrate.sql
+++ migrate.sql
@@ -37,35 +37,7 @@ CREATE TABLE odds (
   over_under SMALLINT
 );
 
-INSERT INTO team (moniker, location, league) VALUES ('Cardinals',   'Arizona',        'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Falcons',     'Atlanta',        'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Ravens',      'Baltimore',      'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Bills',       'Buffalo',        'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Panthers',    'Carolina',       'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Bears',       'Chicago',        'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Bengals',     'Cincinnati',     'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Browns',      'Cleveland',      'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Cowboys',     'Dallas',         'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Broncos',     'Denver',         'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Lions',       'Detroit',        'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Packers',     'Green Bay',      'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Texans',      'Houston',        'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Colts',       'Indianapolis',   'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Jaguars',     'Jacksonville',   'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Chiefs',      'Kansas City',    'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Raiders',     'Las Vegas',      'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Chargers',    'Los Angeles',    'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Rams',        'Los Angeles',    'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Dolphins',    'Miami',          'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Vikings',     'Minnesota',      'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Patriots',    'New England',    'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Saints',      'New Orleans',    'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Giants',      'New York',       'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Jets',        'New York',       'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Eagles',      'Philadelphia',   'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Steelers',    'Pittsburgh',     'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('49ers',       'San Francisco',  'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Seahawks',    'Seattle',        'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Buccaneers',  'Tampa Bay',      'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Titans',      'Tennessee',      'NFL');
-INSERT INTO team (moniker, location, league) VALUES ('Redskins',    'Washington',     'NFL');
+CREATE TABLE team_search_term (
+  team_id INTEGER REFERENCES team,
+  term VARCHAR(255)
+);
