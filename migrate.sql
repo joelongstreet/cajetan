0a0b290e683ed46120da00a0fdd07554530487a2
Joe Longstreet <joelongstreet@gmail.com>
2020-02-29 14:22:44 -0600

    add an elo outcome view for easier analysis

--- migrate.sql
+++ migrate.sql
@@ -48,3 +48,44 @@ CREATE TABLE team_search_term (
   term VARCHAR(255),
   UNIQUE(team_id, term)
 );
+
+create or replace view elo_outcome(elo_predicted_outcome, even_elo, elo_diff, matchup_id, elo_team_a, elo_team_b) as
+SELECT t.team_a_score > t.team_b_score AND t.elo_team_a > t.elo_team_b OR
+       t.team_b_score > t.team_a_score AND t.elo_team_b > t.elo_team_a AS elo_predicted_outcome,
+       t.elo_team_a = t.elo_team_b                                     AS even_elo,
+       abs(t.elo_team_a - t.elo_team_b)                                AS elo_diff,
+       t.matchup_id,
+       t.elo_team_a,
+       t.elo_team_b
+FROM (SELECT matchup.id     AS matchup_id,
+             matchup.team_a_score,
+             matchup.team_b_score,
+             elo_team_a.elo AS elo_team_a,
+             elo_team_b.elo AS elo_team_b
+      FROM matchup
+               LEFT JOIN team team_a ON team_a.id = matchup.team_a_id
+               LEFT JOIN team team_b ON team_b.id = matchup.team_b_id
+               LEFT JOIN LATERAL ( SELECT elo.id,
+                                          elo.team_id,
+                                          elo.as_of,
+                                          elo.elo
+                                   FROM elo
+                                   WHERE elo.as_of < matchup.start_time
+                                     AND elo.team_id = team_a.id
+                                   ORDER BY elo.as_of DESC
+                                   LIMIT 1) elo_team_a ON elo_team_a.team_id = team_a.id
+               LEFT JOIN LATERAL ( SELECT elo.id,
+                                          elo.team_id,
+                                          elo.as_of,
+                                          elo.elo
+                                   FROM elo
+                                   WHERE elo.as_of < matchup.start_time
+                                     AND elo.team_id = team_b.id
+                                   ORDER BY elo.as_of DESC
+                                   LIMIT 1) elo_team_b ON elo_team_b.team_id = team_b.id
+      WHERE matchup.start_time IS NOT NULL
+      ORDER BY matchup.start_time) t
+ORDER BY (abs(t.elo_team_a - t.elo_team_b)) DESC;
+
+alter table elo_outcome owner to postgres;
+
