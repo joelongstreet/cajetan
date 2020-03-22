2fdfd93543ed50ca76cb5d3fb4934c18c9391aa8
Joe Longstreet <joelongstreet@gmail.com>
2020-03-22 14:12:57 -0500

    more predictive polynomial regression

--- analyzer/project/lib/probability.py
+++ analyzer/project/lib/probability.py
@@ -1,10 +1,9 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
 import numpy as np
 import project.sql.pg_client as pg_client
-
-polynomial_regression = PolynomialFeatures(degree=2)
 
 
 def get_independent_range(query):
@@ -47,19 +46,71 @@ def get_logistic_regression_probabilties(query, independent_range):
 
 def get_polynomnial_regression_probabilities(query, independent_range):
     sql_response = pg_client.execute_sql(query)
-    probabilties = map(
-      lambda x: _fill_blanks_with_NaN(x, sql_response), independent_range
+    dependent = list(
+      map(
+        lambda x: _fill_blanks_with_NaN(x, sql_response), independent_range
+      )
     )
 
-    return list(probabilties)
+    progressive_dependent = _fill_NaNs_with_linear_progression(dependent)
+
+    polynomial_regression = PolynomialFeatures(degree=3)
+    polynomial_fit = polynomial_regression.fit_transform(
+      np.reshape(
+        independent_range,
+        (-1, 1)
+      )
+    )
+
+    linear_regression = LinearRegression()
+    linear_regression.fit(
+      polynomial_fit,
+      np.reshape(
+        progressive_dependent,
+        (-1, 1)
+      )
+    )
+
+    probabilties = linear_regression.predict(polynomial_fit)
+
+    return list(
+      zip(*probabilties)
+    )[0]
 
 
-def _fill_blanks_with_NaN(i, dependent_range_array):
+def _fill_blanks_with_NaN(i, independent_range_array):
     probability = [
-      item for item in dependent_range_array if item[0] == i
+      item for item in independent_range_array if item[0] == i
     ]
 
     if (probability):
         return probability[0][1]
     else:
-        return float('NaN')
+        return np.nan
+
+
+def _fill_NaNs_with_linear_progression(lst):
+    ret = lst.copy()
+
+    for i, d in enumerate(ret):
+        if np.isfinite(d):
+            continue
+
+        previous_d = 0
+        if(i > 0):
+            previous_d = ret[i - 1]
+
+        next_d = d
+        next_d_index = i
+        for j, _k in enumerate(ret, start=i+1):
+            dd = ret[j]
+            if np.isfinite(dd):
+                next_d = dd
+                next_d_index = j
+                break
+
+        index_diff = next_d_index - i + 1
+        d_diff = next_d - previous_d
+        ret[i] = previous_d + (d_diff / index_diff)
+
+    return ret
